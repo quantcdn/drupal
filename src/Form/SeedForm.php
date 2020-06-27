52ca686e01fa304374ddde52a614544771fee5b4
Stuart Rowlands <stuart@firecannon.com>
2020-06-28 07:44:27 +1200

    Remove quant params and make relative on redirect.

--- src/Form/SeedForm.php
+++ src/Form/SeedForm.php
@@ -215,7 +215,7 @@ class SeedForm extends FormBase {
       $query = \Drupal::entityQuery('node');
 
       // Restrict by bundle.
-      if (!empty($bundles = array_filter($form_state->getValue('entity_node_bundles')))) {
+      if ($bundles = array_filter($form_state->getValue('entity_node_bundles'))) {
         if (!empty($bundles)) {
           $query->condition('type', array_keys($bundles), 'IN');
         }
