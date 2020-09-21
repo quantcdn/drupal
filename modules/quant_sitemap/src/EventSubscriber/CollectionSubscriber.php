4bf7d4d0cf82b24f35a053b9cd0332f03c863f61
Steve Worley <sj.worley88@gmail.com>
2020-09-21 19:31:14 +1000

    Support xmlsitemap.

--- modules/quant_sitemap/src/EventSubscriber/CollectionSubscriber.php
+++ modules/quant_sitemap/src/EventSubscriber/CollectionSubscriber.php
@@ -5,28 +5,34 @@ namespace Drupal\quant_sitemap\EventSubscriber;
 use Drupal\quant\Event\CollectRoutesEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\quant\Event\QuantCollectionEvents;
-use Drupal\simple_sitemap\SimplesitemapManager;
+use Drupal\Core\Extension\ModuleHandler;
+use Drupal\Core\Entity\EntityTypeManager;
 
+/**
+ * Collection subscriber for Sitemap routes.
+ */
 class CollectionSubscriber implements EventSubscriberInterface {
 
   /**
-   * The entity type manager.
+   * The core module handler.
+   *
+   * @var Drupal\Core\Extension\ModuleHandler
    */
-  protected $manager;
+  protected $moduleHandler;
 
   /**
-   * The config factory.
+   * The entity type manager.
    *
-   * @var Drupal\Core\Config\ConfigFactory
+   * @var Drupal\Core\Entity\EntityTypeManager
    */
-  protected $configFactory;
+  protected $entityTypeManager;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(SimplesitemapManager $sitemap_manager)
-  {
-    $this->manager = $sitemap_manager;
+  public function __construct(ModuleHandler $module_handler, EntityTypeManager $entity_type_manager) {
+    $this->moduleHandler = $module_handler;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -38,6 +44,61 @@ class CollectionSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Get the simple sitemap manager.
+   *
+   * @return Drupal\simple_sitemap\SimplesitemapManager
+   */
+  public function getSitemapManager() {
+    return \Drupal::service('simple_sitemap.manager');
+  }
+
+  /**
+   * The entity type manager.
+   *
+   * @return Drupal\Core\Entity\EntityTypeManager
+   */
+  public function getEntityTypeManager() {
+    return $this->entityTypeManager;
+  }
+
+  /**
+   * Simple sitemap support.
+   *
+   * Simple sitemap allows you to define accessible sitemap routes. This
+   * is always prefixed by the variant ID. As this is not a standard
+   * content entity we need to use the provided manager to load the values.
+   *
+   * @return array
+   *   A list of routes that sitemaps are accessible by.
+   */
+  public function getSimpleSitemapItems() : array {
+    $items = ['/sitemap.xml'];
+    foreach ($this->getSitemapManager()->getSitemapVariants() as $variant => $def) {
+      $items[] = "/$variant/sitemap.xml";
+    }
+    return $items;
+  }
+
+  /**
+   * XMLSitemap support.
+   *
+   * XMLSitemap only allows base level sitemaps with language prefixes.
+   * Each enabled language context can only have one sitemap, the admin
+   * UI returns an error if you try to double up.
+   *
+   * @return array
+   *   A list of routes that sitemaps are accessible by.
+   */
+  public function getXmlsitemapItems() : array {
+    $items = [];
+    $sitemaps = $this->getEntityTypeManager()->getStorage('xmlsitemap')->loadMultiple();
+    foreach ($sitemaps as $sitemap) {
+      $items[] = "/{$sitemap->language()->getId()}/sitemap.xml";
+    }
+    return $items;
+  }
+
+  /**
    * Collect the sitemap routes.
    */
   public function collectRoutes(CollectRoutesEvent $event) {
@@ -45,9 +106,17 @@ class CollectionSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    $event->addRoute('/sitemap.xml');
-    foreach ($this->manager->getSitemapVariants() as $variant => $def) {
-      $event->addRoute("/$variant/sitemap.xml");
+    $items = [];
+
+    if ($this->moduleHandler->moduleExists('simple_sitemap')) {
+      $items = $this->getSimpleSitemapItems();
+    }
+    else if ($this->moduleHandler->moduleExists('xmlsitemap')) {
+      $items = $this->getXmlsitemapItems();
+    }
+
+    foreach ($items as $route) {
+      $event->addRoute($route);
     }
   }
 
