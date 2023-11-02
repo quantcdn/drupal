9e8e903d5fd12a97dc605b7ff1d10bbacd17eb4c
Kristen Pol <kristen.pol@gmail.com>
2023-11-02 13:28:10 +1100

    Updated variable usage to get rid of warning.

--- modules/quant_purger/src/Controller/QuantPurgerController.php
+++ modules/quant_purger/src/Controller/QuantPurgerController.php
@@ -24,10 +24,10 @@ class QuantPurgerController extends ControllerBase {
     }
 
     if (\Drupal::moduleHandler()->moduleExists('purge_ui')) {
-      $text .= $this->t('You can add and update the Quant Purger settings via the <a href=":url">Purge UI</a>.<br/><br/>', [':url' => '/admin/config/development/performance/purge']);
+      $text = $this->t('You can add and update the Quant Purger settings via the <a href=":url">Purge UI</a>.<br/><br/>', [':url' => '/admin/config/development/performance/purge']);
     }
     else {
-      $text .= $this->t('Enabling the <a href=":url">Purge UI module</a> will allow you to configure the Quant Purger module through the UI.<br/><br/>', [':url' => 'https://www.drupal.org/project/purge']);
+      $text = $this->t('Enabling the <a href=":url">Purge UI module</a> will allow you to configure the Quant Purger module through the UI.<br/><br/>', [':url' => 'https://www.drupal.org/project/purge']);
     }
     $text .= $this->t('For more information about the Quant Purger module, refer to the <a href=":url">documentation</a>.', [':url' => 'https://docs.quantcdn.io/docs/integrations/drupal/purger']);
     $build = [
