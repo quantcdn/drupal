7cec726e24e271a0995ddcef7baad9b4ad838c96
Kristen Pol <kristen.pol@gmail.com>
2023-12-02 15:57:28 +1100

    Removed metadata redirect route change.

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -19,15 +19,6 @@ class RouteSubscriber extends RouteSubscriberBase {
     if ($route = $collection->get('entity.node.canonical')) {
       $route->setDefault('_controller', '\Drupal\quant\Controller\QuantNodeViewController::view');
     }
-
-    // Hide metadata redirect route if redirect module isn't installed.
-    if ($route = $collection->get('quant.metadata_redirect')) {
-      if (!\Drupal::moduleHandler()->moduleExists('redirect')) {
-        // Note second parameter is a string.
-        $route->setRequirement('_access', 'FALSE');
-      }
-    }
-
   }
 
 }
